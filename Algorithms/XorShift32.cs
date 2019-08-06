7300cb8dfe0d8ec8bb3a27d70b1a40c096bf8a06
Adam Łyskawa <adam@codedog.pl>
2019-08-06 22:25:14 +0200

    FIX: Bugs in XorShift32 algorithms.

--- Algorithms/XorShift32.cs
+++ Algorithms/XorShift32.cs
@@ -24,9 +24,9 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than System.Int32.MaxValue.</returns>
         public int Next() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return unchecked((int)(Seed & 0x7fffffff));
         }
 
@@ -38,9 +38,9 @@ namespace Woof.Algorithms {
         public int Next(int max) {
             if (max < 0) throw new ArgumentException();
             if (max < 1) return 0;
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (int)(Seed / Max * max);
         }
 
@@ -52,11 +52,11 @@ namespace Woof.Algorithms {
         /// <returns>A 32-bit signed integer greater than or equal to minValue and less than maxValue; that is, the range of return values includes minValue but not maxValue. If minValue equals maxValue, minValue is returned.</returns>
         public int Next(int min, int max) {
             if (min == max) return min;
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             if (min == int.MinValue && max == int.MaxValue) return unchecked((int)Seed);
-            return (int)(min + (Seed / Max * ((double)max - (double)min)));
+            return min + (int)(Seed / Max * ((double)max - (double)min));
         }
 
         /// <summary>
@@ -64,10 +64,11 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A double-precision floating point number that is greater than or equal to 0.0 and less than 1.0.</returns>
         public double NextDouble() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed << 17;
-            Seed ^= Seed << 5;
-            return Seed / Max;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
+            var seed52 = ((long)Seed << 21) & DM | D1; // we use 32 of 52 bits of double fraction part
+            return BitConverter.Int64BitsToDouble(seed52) - 1.0;
         }
 
         /// <summary>
@@ -75,9 +76,9 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A random boolean value.</returns>
         public bool NextBool() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (Seed & 1) != 0;
         }
 
@@ -88,9 +89,9 @@ namespace Woof.Algorithms {
         public byte NextByte() {
             BCS = (BC++ % 4) << 3;
             if (BCS != 0) return (byte)(Seed >> BCS & 0xff);
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (byte)(Seed & 0xff);
         }
 
@@ -103,9 +104,9 @@ namespace Woof.Algorithms {
             int n = buffer.Length;
             int m = n & 0x7ffffffc;
             while (i < m) {
-                Seed ^= Seed << 13;
-                Seed ^= Seed >> 17;
-                Seed ^= Seed << 5;
+                Seed ^= Seed << S1;
+                Seed ^= Seed >> S2;
+                Seed ^= Seed << S3;
                 buffer[i++] = (byte)(Seed >> 24 & 0xff);
                 buffer[i++] = (byte)(Seed >> 16 & 0xff);
                 buffer[i++] = (byte)(Seed >> 8 & 0xff);
@@ -114,16 +115,28 @@ namespace Woof.Algorithms {
             for (; i < n; i++) {
                 BCS = (i % 4) << 3;
                 if (BCS != 0) { buffer[i] = (byte)(Seed >> BCS & 0xff); continue; }
-                Seed ^= Seed << 13;
-                Seed ^= Seed >> 17;
-                Seed ^= Seed << 5;
+                Seed ^= Seed << S1;
+                Seed ^= Seed >> S2;
+                Seed ^= Seed << S3;
                 buffer[i] = (byte)(Seed & 0xff);
             }
         }
 
+        #region Constants
+
+        const int
+            S1 = 13,
+            S2 = 17,
+            S3 = 5;
+        const long
+            D1 = 0b0_01111111111_0000000000000000000000000000000000000000000000000000, // binary for 1.0 (2 ** 0)
+            DM = 0b0_00000000000_1111111111111111111111111111111111111111111111111111; // binary mask for fraction part (0.(9))
+        const double Max = uint.MaxValue;
+
+        #endregion
+
         #region Private data
 
-        const double Max = uint.MaxValue;
         uint Seed;
         int BC;
         int BCS;
--- Algorithms/XorShift32T.cs
+++ Algorithms/XorShift32T.cs
@@ -3,7 +3,7 @@
 namespace Woof.Algorithms {
 
     /// <summary>
-    /// TURBO 32-bit pseudo-random number generator.
+    /// TURBO 32-bit pseudo-random number generator. Utilizes unsafe operations for speed.
     /// </summary>
     /// <remarks>
     /// Optimized for speed on x64 and x86 systems.
@@ -16,7 +16,7 @@ namespace Woof.Algorithms {
         /// <param name="seed">32-bit seed value, leave 0 for random seed.</param>
         public XorShift32T(int seed = 0) {
             Seed = seed;
-            while (Seed == 0) Seed = unchecked((int)DateTime.UtcNow.Ticks);
+            while (Seed == 0) Seed = Seed = unchecked((int)DateTime.UtcNow.Ticks);
         }
 
         /// <summary>
@@ -24,10 +24,10 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than System.Int32.MaxValue.</returns>
         public int Next() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
-            return Seed & 0x7fffffff;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
+            return Seed & Int32.MaxValue;
         }
 
         /// <summary>
@@ -38,9 +38,9 @@ namespace Woof.Algorithms {
         public int Next(int max) {
             if (max < 0) throw new ArgumentException();
             if (max < 1) return 0;
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (int)(Seed / Max * max);
         }
 
@@ -52,11 +52,11 @@ namespace Woof.Algorithms {
         /// <returns>A 32-bit signed integer greater than or equal to minValue and less than maxValue; that is, the range of return values includes minValue but not maxValue. If minValue equals maxValue, minValue is returned.</returns>
         public int Next(int min, int max) {
             if (min == max) return min;
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
-            if (min == int.MinValue && max == int.MaxValue) return unchecked((int)Seed);
-            return (int)(min + ((uint)Seed / Max * ((double)max - (double)min)));
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
+            if (min == int.MinValue && max == int.MaxValue) return Seed;
+            return min + (int)((Seed & 0x7fffffff) / Max * ((double)max - (double)min));
         }
 
         /// <summary>
@@ -64,10 +64,15 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A double-precision floating point number that is greater than or equal to 0.0 and less than 1.0.</returns>
         public double NextDouble() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed << 17;
-            Seed ^= Seed << 5;
-            return Seed / Max;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
+            var seed52 = ((long)Seed << 21) & DM | D1; // we use 32 of 52 bits of double fraction part
+            unsafe {
+                void* vp = &seed52;
+                var dp = (double*)vp;
+                return *dp - 1.0;
+            }
         }
 
         /// <summary>
@@ -75,9 +80,9 @@ namespace Woof.Algorithms {
         /// </summary>
         /// <returns>A random boolean value.</returns>
         public bool NextBool() {
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (Seed & 1) != 0;
         }
 
@@ -88,9 +93,9 @@ namespace Woof.Algorithms {
         public byte NextByte() {
             BCS = (BC++ % 4) << 3;
             if (BCS != 0) return (byte)(Seed >> BCS & 0xff);
-            Seed ^= Seed << 13;
-            Seed ^= Seed >> 17;
-            Seed ^= Seed << 5;
+            Seed ^= Seed << S1;
+            Seed ^= Seed >> S2;
+            Seed ^= Seed << S3;
             return (byte)(Seed & 0xff);
         }
 
@@ -106,26 +111,38 @@ namespace Woof.Algorithms {
                 int* u = (int*)p;
                 int* v = u + m;
                 while (u < v) {
-                    Seed ^= Seed << 13;
-                    Seed ^= Seed >> 17;
-                    Seed ^= Seed << 5;
+                    Seed ^= Seed << S1;
+                    Seed ^= Seed >> S2;
+                    Seed ^= Seed << S3;
                     *(u++) = Seed;
                 }
             }
             for (i = m << 2; i < n; i++) {
                 BCS = (i % 4) << 3;
                 if (BCS != 0) { buffer[i] = (byte)(Seed >> BCS & 0xff); continue; }
-                Seed ^= Seed << 13;
-                Seed ^= Seed >> 17;
-                Seed ^= Seed << 5;
+                Seed ^= Seed << S1;
+                Seed ^= Seed >> S2;
+                Seed ^= Seed << S3;
                 buffer[i] = (byte)(Seed & 0xff);
             }
 
         }
 
+        #region Constants
+
+        const int
+            S1 = 13,
+            S2 = 17,
+            S3 = 5;
+        const long
+            D1 = 0b0_01111111111_0000000000000000000000000000000000000000000000000000, // binary for 1.0 (2 ** 0)
+            DM = 0b0_00000000000_1111111111111111111111111111111111111111111111111111; // binary mask for fraction part (0.(9))
+        const double Max = Int32.MaxValue;
+
+        #endregion
+
         #region Private data
 
-        const double Max = uint.MaxValue;
         int Seed;
         int BC;
         int BCS;
